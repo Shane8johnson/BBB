e0aae3e7645aef35b7aa4d44ffa645a5e95e17e0
Shane8johnson <36906111+Shane8johnson@users.noreply.github.com>
2021-02-06 15:30:57 -0600

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -1,6 +1,16 @@
 <?php
-header("Location: /plugin.php?plugin=fpp-BigBeautifulButtons&page=bigbeautifulbuttons.php&nopage=1"); /* Redirect browser */
-
-/* Make sure that code below does not get executed when we redirect. */
-exit;
 ?>
+<html>
+<head>
+</head>
+<body>
+<h2 style="text-align: center;"><span style="color: #e03e2d;">Server Connection Check:</span></h2>
+<h2 style="text-align: center;"><span style="color: #e03e2d;">If you can see this page, click the button below.</span></h2>
+<p style="text-align: center;"><a href="/plugin.php?plugin=fpp-BigBeautifulButtons&page=bigbeautifulbuttons.php&nopage=1"><img src="https://dabuttonfactory.com/button.png?t=I+am+not+a+Robot&amp;f=Open+Sans-Bold&amp;ts=26&amp;tc=fff&amp;hp=45&amp;vp=20&amp;c=11&amp;bgt=gradient&amp;bgc=f00&amp;ebgc=900" alt="" width="303" height="66" /></a></p>
+<base target="_parent">
+
+<script>
+window.top.location.href = "/plugin.php?plugin=fpp-BigBeautifulButtons&page=bigbeautifulbuttons.php&nopage=1"; 
+</script>
+</body>
+</html>
